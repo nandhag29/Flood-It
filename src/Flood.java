c914f5b2b2ddb996ae6be98fda5922641de9e441
nagopi <nagopi@iu.edu>
2023-09-11 16:42:09 -0400

    flood and flood1 implementations

--- src/Flood.java
+++ src/Flood.java
@@ -5,9 +5,42 @@ import java.util.*;
 
 
 public class Flood {
-    public static void flood1(WaterColor color, LinkedList<Coord> flooded_list, Tile[][] tiles, Integer board_size) {
-        //O(n) time complexity
 
+    // An alternative implementation goes here.
+    //O(n) time complexity
+    public static void flood(WaterColor color, LinkedList<Coord> flooded_list, Tile[][] tiles, Integer board_size) {
+        boolean[][] visited = new boolean[board_size][board_size];
+        LinkedList<Coord> queue = new LinkedList<>();
+
+        for (Coord coord : flooded_list) {
+            queue.add(coord);
+            visited[coord.getY()][coord.getX()] = true;
+        }
+
+        while (!queue.isEmpty()) {
+            Coord current = queue.poll();
+
+            int[][] neighborsOffsets = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+            for (int[] offset : neighborsOffsets) {
+                int newY = current.getY() + offset[0];
+                int newX = current.getX() + offset[1];
+
+                if (newY >= 0 && newY < board_size && newX >= 0 && newX < board_size) {
+                    Coord neighbor = new Coord(newX, newY);
+                    if (!visited[newY][newX] && tiles[newY][newX].getColor() == color) {
+                        visited[newY][newX] = true;
+                        flooded_list.add(neighbor);
+                        queue.add(neighbor);
+                    }
+                }
+            }
+        }
+    }
+
+    // Students implement this flood function.
+    //O(n) time complexity
+    public static void flood1(WaterColor color, LinkedList<Coord> flooded_list, Tile[][] tiles, Integer board_size) {
         LinkedList<Coord> queue = new LinkedList<>();
         boolean[][] visited = new boolean[board_size][board_size];
 
@@ -27,35 +60,6 @@ public class Flood {
                 }
             }
         }
-    }
-
-    public static void flood(WaterColor color, LinkedList<Coord> flooded_list, Tile[][] tiles, Integer board_size) {
-        //O(n) time complexity
-
-        boolean[][] visited = new boolean[board_size][board_size];
-
-        for (Coord coord : flooded_list) {
-            visited[coord.getY()][coord.getX()] = true;
-        }
-
-        LinkedList<Coord> newCoords = new LinkedList<>();
-
-        for (int y = 0; y < board_size; y++) {
-            for (int x = 0; x < board_size; x++) {
-                if (visited[y][x] && tiles[y][x].getColor() == color) {
-                    Coord current = new Coord(x, y);
-
-                    for (Coord neighbor : current.neighbors(board_size)) {
-                        if (!visited[neighbor.getY()][neighbor.getX()]) {
-                            visited[neighbor.getY()][neighbor.getX()] = true;
-                            newCoords.add(neighbor);
-                        }
-                    }
-                }
-            }
-        }
-
-        flooded_list.addAll(newCoords);
     }
 
 }
