8ed55b83e490bcf21d7bee3d46ed7dc056efceaf
nagopi <nagopi@iu.edu>
2023-09-11 10:42:58 -0400

    flood and flood1 implementations

--- src/Flood.java
+++ src/Flood.java
@@ -7,10 +7,7 @@ import java.util.*;
 public class Flood {
 
     // Students implement this flood function.
-    public static void flood(WaterColor color,
-                             LinkedList<Coord> flooded_list,
-                             Tile[][] tiles,
-                             Integer board_size) {
+    public static void flood(WaterColor color, LinkedList<Coord> flooded_list, Tile[][] tiles, Integer board_size) {
         LinkedList<Coord> queue = new LinkedList<>();
         boolean[][] visited = new boolean[board_size][board_size];
 
@@ -33,11 +30,31 @@ public class Flood {
     }
 
     // An alternative implementation goes here.
-    public static void flood1(WaterColor color,
-                             LinkedList<Coord> flooded_list,
-                             Tile[][] tiles,
-                             Integer board_size) {
-        // YOUR CODE
+    public static void flood1(WaterColor color, LinkedList<Coord> flooded_list, Tile[][] tiles, Integer board_size) {
+        boolean[][] visited = new boolean[board_size][board_size];
+
+        for (Coord coord : flooded_list) {
+            visited[coord.getY()][coord.getX()] = true;
+        }
+
+        LinkedList<Coord> newCoords = new LinkedList<>();
+
+        for (int y = 0; y < board_size; y++) {
+            for (int x = 0; x < board_size; x++) {
+                if (visited[y][x] && tiles[y][x].getColor() == color) {
+                    Coord current = new Coord(x, y);
+
+                    for (Coord neighbor : current.neighbors(board_size)) {
+                        if (!visited[neighbor.getY()][neighbor.getX()]) {
+                            visited[neighbor.getY()][neighbor.getX()] = true;
+                            newCoords.add(neighbor);
+                        }
+                    }
+                }
+            }
+        }
+
+        flooded_list.addAll(newCoords);
     }
 
 }
