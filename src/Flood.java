515eec3ed94cb00d5466c802f13e3ac9f6f59c64
nagopi <nagopi@iu.edu>
2023-09-11 01:46:41 -0400

    commit

--- src/Flood.java
+++ src/Flood.java
@@ -7,11 +7,30 @@ import java.util.*;
 public class Flood {
 
     // Students implement this flood function.
-    public static void flood(WaterColor color, LinkedList<Coord> flooded_list, Tile[][] tiles, Integer board_size) {
+    public static void flood(WaterColor color,
+                             LinkedList<Coord> flooded_list,
+                             Tile[][] tiles,
+                             Integer board_size) {
+        LinkedList<Coord> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[board_size][board_size];
 
+        for (Coord coord : flooded_list) {
+            queue.add(coord);
+            visited[coord.getY()][coord.getX()] = true;
         }
 
+        while (!queue.isEmpty()) {
+            Coord current = queue.removeFirst();
 
+            for (Coord neighbor : current.neighbors(board_size)) {
+                if (!visited[neighbor.getY()][neighbor.getX()] && tiles[neighbor.getY()][neighbor.getX()].getColor() == color) {
+                    visited[neighbor.getY()][neighbor.getX()] = true;
+                    flooded_list.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+    }
 
     // An alternative implementation goes here.
     public static void flood1(WaterColor color,
